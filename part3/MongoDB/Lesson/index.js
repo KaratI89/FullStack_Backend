3afcf7aa82a46e4d918f59c813c0c014172562eb
Korotaev Ilia <korotaevia@biocad.ru>
2024-04-17 13:42:53 +0300

    fix

--- part3/MongoDB/Lesson/index.js
+++ part3/MongoDB/Lesson/index.js
@@ -1,93 +1,101 @@
-const express = require('express') // определяться должно только так иначе будет ошибка
-const morgan = require('morgan')
+const express = require('express')
 const cors = require('cors')
 const app = express()
 
-morgan.token('body', req => {
-  return JSON.stringify(req.body)
-})
-app.use(express.json())
-app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
-app.use(cors())
+const requestLoger = (req, res, next) => {
+    console.log('Method:', req.method);
+    console.log('Path:', req.path);
+    console.log('Body:', req.body);
+    console.log('---');
+    next()
+}
 
-let people = [
-    { 
+const unknownEndepoint = (req, res) => {
+    res.status(400).send({error: 'unknown endepoint'})
+}
+
+app.use(express.json())
+app.use(requestLoger)
+app.use(cors())
+app.use(express.static('dist'))
+
+let notes = [
+    {
         id: 1,
-        name: "Arto Hellas", 
-        number: "040-123456"
+        content: "HTML is easy",
+        important: true
     },
-    { 
+    {
         id: 2,
-        name: "Ada Lovelace", 
-        number: "39-44-5323523"
+        content: "Browser can execute only JavaScript",
+        important: false
     },
-    { 
+    {
         id: 3,
-        name: "Dan Abramov", 
-        number: "12-43-234345"
-    },
-    { 
-        id: 4,
-        name: "Mary Poppendieck", 
-        number: "39-23-6423122"
+        content: "GET and POST are the most impottant mithods of HTTP protocol",
+        important: true
     }
 ]
-
-app.get('/api/persons', (request, response) => {
-    response.json(people)
-})
-app.get('/info', (request, response) =>{
-    const getPersonNumber = people.length
-    const getData = new Date()
-    response.send(`Phone book has info for ${getPersonNumber} people</br> ${getData}`)
+app.get('/', (request, response) => {
+    response.send('<h1>Hello</h1>')
 })
 
-app.get('/api/person/:id', (request, response) => {
+app.get('/api/notes', (request, response) => {
+  response.json(notes)
+})
+
+app.get('/api/notes/:id', (request, response) => {
     const id = Number(request.params.id)
-    person = people.find(person=> person.id === id)
-    if (person) {
-        return response.json(person)
+    console.log(id);
+    const note = notes.find(note => {
+        console.log(note.id, typeof note.id, typeof id, note.id === id);
+        return note.id === id
+    })
+    if (note) {
+        response.json(note)
     }
     else {
-        response.statusMessage = 'Person not found'
+        response.statusMessage = "Not found message"
         response.status(404).end()
     }
+    console.log(note);
+    //response.json(note) повторный вызов  давал ошибку: Cannot set headers after they are sent to the client
 })
 
-app.delete('/api/person/:id', (request, response) => {
+app.delete('/api/notes/:id', (request, response) => {
     const id = Number(request.params.id)
-    people = people.filter(person=> person.id !== id)
+    console.log(id);
+    notes = notes.filter(note => note.id !== id)
+    console.log(notes);
     response.status(204).end()
 })
 
-const getRandomId = () => {
-    const min = Math.ceil(1)
-    const max = Math.floor(999999)
-    return Math.floor(Math.random() * (max - min) + min)
+const generatedId = () => {
+    const maxId = notes.length > 0 
+        ? Math.max(...notes.map(n => n.id))
+        : 0
+    return maxId + 1
 }
 
-app.post('/api/persons', (request, response) => {
-    const personData = request.body
-    //console.log(people.find(person => person.name === personData.name));
-    if(
-        !personData.name 
-        || !personData.number 
-        || people.find(person => person.name === personData.name)
-        || people.find(person => person.number === personData.number)
-    ) {
-        return response.status(400).json({error: 'content is wrong'})
+app.post('/api/notes', (request, response) =>{
+    const body = request.body
+
+    if (!body.content) {
+        return response.status(400).json({
+            error: 'content missing'
+        })
     }
-    const person = {
-        id: getRandomId(),
-        name: personData.name ,
-        number: personData.number
+    const note = {
+        content: body.content,
+        important: body.important || false,
+        id: generatedId(),
     }
-    people = people.concat(person)
-    response.json(person)
+    notes = notes.concat(note)
+    response.json(note)
 })
+app.use(unknownEndepoint)
 
-
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
