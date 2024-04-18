702fb5d464d1a6feb62136b6c469df1e2be32d9d
Korotaev Ilia <korotaevia@biocad.ru>
2024-04-18 16:17:17 +0300

    started exercise 3.12

---
+++ part3/MongoDB/phonebook/mongo.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length<3) {
+  console.log('give password as argument')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+const name = process.argv[3]
+const number = process.argv[4]
+
+const url = `mongodb+srv://karat0zero:${password}@cluster0.5ktkdqh.mongodb.net/phonebookApp?retryWrites=true&w=majority`
+
+  mongoose.set('strictQuery',false)
+
+  mongoose.connect(url)
+
+  const noteSchema = new mongoose.Schema({
+      name: String,
+      number: String,
+  })
+
+  const Person = mongoose.model('Person', noteSchema)
+
+  const person = new Person({
+        name:   name,
+        number: number,
+  })
+
+  person.save().then(result => {
+    console.log(`added ${name} number ${number} to phonebook`)
+    mongoose.connection.close()
+  })
+  // Person.find({}).then(result => {
+  //   result.forEach(person => {
+  //     console.log(person)
+  //   })
+  //   mongoose.connection.close()
+  // })
