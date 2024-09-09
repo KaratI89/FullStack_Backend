ee5273298a6c32cf0527b377c31735804c06c9e9
KaratI89 <karat0zero@gmail.com>
2024-09-09 20:56:28 +0300

    exercises 4.3-4.7 are in progress

--- part4/structure_of_backend_application/blog_list_application/utils/list_helper.js
+++ part4/structure_of_backend_application/blog_list_application/utils/list_helper.js
@@ -6,11 +6,22 @@ const totalLikes = (blogs) => {
   const reducer = (sum, item) => {
     console.log(sum);
     console.log(item.likes);
-    
-    
     return sum + item.likes
   }
-  return blogs.reduce(reducer,0)
+  
+  return blogs.length === 0 
+  ? 0
+  : blogs.reduce(reducer,0)
 }
 
-module.exports = { dummy , totalLikes }
+const favoriteBlog = (blogs) => {
+  const reducer  = (previous, current) => {
+    console.log('prev',previous );
+    console.log('cur', current);
+    return current.likes > previous.likes ? current : previous
+  }
+  console.log(blogs.reduce(reducer,0));
+    return blogs.reduce(reducer,0)
+}
+
+module.exports = { dummy , totalLikes, favoriteBlog }
