5cc7515a85e887c5902450e0050e80b3c844fbbe
KaratI89 <karat0zero@gmail.com>
2024-09-17 22:06:46 +0300

    in the process of supertest, notes was created in MongoDB

--- part4/structure_of_backend_application/lesson/mongo.js
+++ part4/structure_of_backend_application/lesson/mongo.js
@@ -7,43 +7,35 @@ if (process.argv.length<3) {
 
 const password = process.argv[2]
 
-const url = `mongodb+srv://karat0zero:${password}@cluster0.5ktkdqh.mongodb.net/noteApp?retryWrites=true&w=majority`
+const url = `mongodb+srv://karat0zero:${password}@cluster0.5ktkdqh.mongodb.net/testNoteApp?retryWrites=true&w=majority`
 
 mongoose.set('strictQuery',false)
 
 mongoose.connect(url)
 
 const noteSchema = new mongoose.Schema({
-  notes: [
-    {
-      content: String,
-      important: Boolean,
-    },
-  ]
+
+  content: String,
+  important: Boolean,
+
 })
 
 const Note = mongoose.model('Note', noteSchema)
 
-const note = new Note({
-  notes: [
-    {
-      content: 'HTML is easy',
-      important: true,
-    },
-    {
-      content: 'MongoDb is fucking shit',
-      important: true,
-    }
-  ]
-})
+const note = new Note(
+  {
+    content: 'Mongo DB is the best',
+    important: true,
+  }
+)
 
-// note.save().then(result => {
-//   console.log('note saved!')
-//   mongoose.connection.close()
-// })
-Note.find({}).then(result => {
-  result.forEach(note => {
-    console.log(note)
-  })
+note.save().then(result => {
+  console.log('note saved!')
   mongoose.connection.close()
 })
+//Note.find({}).then(result => {
+//  result.forEach(note => {
+//    console.log(note)
+//  })
+//  mongoose.connection.close()
+//})
