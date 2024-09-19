31ea3a14d113acafce0586b01199589120f2cce2
Korotaev Ilia <korotaevia@biocad.ru>
2024-09-19 16:23:26 +0300

    supertest chapter is finished

--- part4/structure_of_backend_application/lesson/tests/note_api.test.js
+++ part4/structure_of_backend_application/lesson/tests/note_api.test.js
@@ -2,6 +2,7 @@ const { test, after } = require('node:test')
 const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
+const assert = require('node:assert')
 
 const api = supertest(app)
 
@@ -10,6 +11,17 @@ test('notes are returned as json', async () => {
     .get('/api/notes')
     .expect(200)
     .expect('Content-Type', /application\/json/)
+})
+
+test('there are two notes', async () => {
+  const response = await api.get('/api/notes')
+  assert.strictEqual(response.body.length, 2)
+})
+
+test('the first note is about HTTP methodes', async () => {
+  const response = await api.get('/api/notes')
+  const contents = response.body.map(e => e.content)
+  assert.strictEqual(contents.includes('HTML is easy'))
 })
 
 after(async () => {
