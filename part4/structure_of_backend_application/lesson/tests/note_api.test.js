064732e30ee2a2b0416810d9735f416a9183dfbb
KaratI89 <karat0zero@gmail.com>
2024-09-20 19:24:17 +0300

    stoped at More tests and refactoring, on the creating of test_helper function

--- part4/structure_of_backend_application/lesson/tests/note_api.test.js
+++ part4/structure_of_backend_application/lesson/tests/note_api.test.js
@@ -26,14 +26,14 @@ beforeEach(async () => {
   await noteObject.save()
 })
 
-test.only('notes are returned as json', async () => {
+test('notes are returned as json', async () => {
   await api
     .get('/api/notes')
     .expect(200)
     .expect('Content-Type', /application\/json/)
 })
 
-test.only('there are two notes', async () => {
+test('there are two notes', async () => {
   const response = await api.get('/api/notes')
   assert.strictEqual(response.body.length, initialNotes.length)
 })
@@ -42,6 +42,43 @@ test('the first note is about HTTP methodes', async () => {
   const response = await api.get('/api/notes')
   const contents = response.body.map(e => e.content)
   assert(contents.includes(initialNotes[0].content))
+})
+
+test('a valid note can be added', async () => {
+  const newNote = {
+    content: 'async/await simplifies making async calls',
+    important: true,
+  }
+
+  await api
+    .post('/api/notes')
+    .send(newNote)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+
+  const response = await api.get('/api/notes')
+
+  const contents = response.body.map(r => r.content)
+
+  assert.strictEqual(response.body.length, initialNotes.length + 1)
+
+  assert(contents.includes('async/await simplifies making async calls'))
+})
+
+test.only('note without content is not added', async () => {
+  const newNote = {
+    important: true
+  }
+
+  await api
+    .post('/api/notes')
+    .send(newNote)
+    .expect(400)
+
+  const response = await api.get('/api/notes/')
+
+  assert.strictEqual(response.body.length, initialNotes.length)
+
 })
 
 after(async () => {
