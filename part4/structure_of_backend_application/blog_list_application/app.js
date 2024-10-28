14d9df1aef435d8cfad12fa5497019c01cd7659b
KaratI89 <karat0zero@gmail.com>
2024-10-28 21:36:52 +0300

    the exercise 4.18 was done

--- part4/structure_of_backend_application/blog_list_application/app.js
+++ part4/structure_of_backend_application/blog_list_application/app.js
@@ -6,6 +6,7 @@ const logger = require ('./utils/logger')
 require ('express-async-errors')
 const blogsRouter = require('./controller/blogs')
 const usersRouter = require('./controller/users')
+const loginRouter = require('./controller/login')
 const middleware = require('./utils/middleware')
 const mongoose = require('mongoose')
 
@@ -19,6 +20,7 @@ const mongoose = require('mongoose')
 
   app.use('/api/blogs', blogsRouter)
   app.use('/api/users', usersRouter)
+  app.use('/api/login', loginRouter)
 
   app.use(middleware.unknownEndpoint)
   app.use(middleware.errorHandler)
